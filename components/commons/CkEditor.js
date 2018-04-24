7e572b7558a525ed5595f85914ed49bcaf1780a9
Blanksoon <farm1771@gmail.com>
2018-04-24 10:51:08 +0700

    fix

--- components/commons/CkEditor.js
+++ components/commons/CkEditor.js
@@ -48,7 +48,7 @@ class CkEditor extends Component {
             afterPaste: this.afterPaste,
             change: this.props.handleOnchangeEditor,
           }}
-          scriptUrl="http://localhost:8081/static/ckeditor/ckeditor.js"
+          scriptUrl="https://admin.maxmuaythai.com//static/ckeditor/ckeditor.js"
         />
       )
     }
